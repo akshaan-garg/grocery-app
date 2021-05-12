e5b092dcd7b8c202f7a83bcccebbbb5e9073b99c
gargaksh <gargaksh@msu.edu>
2021-05-12 23:02:02 +0530

    Initial log in page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grocery shop log in</title>
+    <link href="index.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<form id="signin" method="post" action="">
+    <img src="png\bag.png" height="200" width="200" alt="logo">
+    <p>Welcome to the Grocery Shop!</p>
+    <table>
+        <tr>
+            <td><label for="email">E-mail: </label></td>
+            <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td><label for="pwd">Password: </label></td>
+            <td><input type="password" name="pwd" id="pwd"></td>
+        </tr>
+    </table>
+    <p><input class="button" type="submit" value="Log in"></p>
+</form>
+<div class="footer"><p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+    <a href='https://pngtree.com/so/bag-clipart'>bag clipart png from pngtree.com</a></div>
+</body>
+</html>
